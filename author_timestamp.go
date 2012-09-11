f348c58fb14a2f568b5f52441a64ef4c19ccf5d5
Michael Bosworth <michael.a.bosworth@gmail.com>
2012-09-10 23:54:33 -0400

    Add author_timestamp struct for tags and commits.

---
+++ author_timestamp.go
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Michael Anselm Bosworth
+ * Due to a perverse and unpleasing character, Michael has not commented this class.
+ */
+package ggit
+
+//some of the fields in commits and tags have an author-with-timestamp value.
+type AuthorTimestamp struct {
+	author string
+	timestamp int64
+}
+
