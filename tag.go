e3f25bf69a11eb692f5a280e52dc91a94d92b5f5
Michael Bosworth <michael.a.bosworth@gmail.com>
2012-09-10 23:55:01 -0400

    Tag stub implementation

---
+++ tag.go
@@ -0,0 +1,37 @@
+/**
+ *
+ * @author Michael Anselm Bosworth
+ * Due to a perverse and unpleasing character, Michael has not commented this class.
+ */
+package ggit
+
+import (
+	"io"
+)
+
+type Tag struct {
+	repo *Repository
+	//commit-SHA1
+	object ObjectId
+	//tag name
+	tag string
+	//author with timestamp
+	tagger *AuthorTimestamp
+	//message
+	message string
+}
+
+func (t *Tag) String() string {
+	//TODO
+	return ""
+}
+
+func (t *Tag) Type() ObjectType {
+	return OBJECT_TAG
+}
+
+func (t *Tag) WriteTo(w io.Writer) (n int, err error) {
+	return io.WriteString(w, string(t.String()))
+}
+
+
