d730b6c4f924dc43f718d5c97ae1610ad9123dc0
Jake Brukhman <jbrukh@gmail.com>
2012-09-19 21:30:00 -0400

    Moved new object type code to object_type.go.

--- objects.go
+++ objects.go
@@ -6,42 +6,8 @@ import (
 )
 
 // ================================================================= //
-// CONSTANTS RELATED TO TYPES
+// OBJECT HEADER PARSING
 // ================================================================= //
-
-// the types of Git objects
-type ObjectType string
-
-// return a human-readable representation of an ObjectType
-func (otype ObjectType) String() string {
-    return string(otype)
-}
-
-const (
-    ObjectBlob   ObjectType = "blob"
-    ObjectTree   ObjectType = "tree"
-    ObjectCommit ObjectType = "commit"
-    ObjectTag    ObjectType = "tag"
-)
-
-var objectTypes []string = []string{
-    ObjectBlob.String(),
-    ObjectTree.String(),
-    ObjectCommit.String(),
-    ObjectTag.String(),
-}
-
-// ================================================================= //
-// OBJECTS AND RAWOBJECTS
-// ================================================================= //
-
-type Object interface {
-    Type() ObjectType
-
-    // write the string representation of 
-    // this object to the writer
-    WriteTo(w io.Writer) (n int, err error)
-}
 
 // ObjectHeader is the deserialized (and more efficiently stored)
 // version of a git object header
@@ -60,24 +26,3 @@ func parseObjectHeader(buf *bufio.Reader) (*objectHeader, error) {
     })
     return h, err
 }
-
-/*
-func parseObjectHeader(b []byte) (typeStr, sizeStr string, pInx uint) {
-    const MAX_SZ = 32
-    var i, j uint
-    l := uint(min(MAX_SZ, len(b)))
-    for i = 0; i < l; i++ {
-        if b[i] == SP {
-            typeStr = string(b[:i])
-            for j = i; j < l; j++ {
-                if b[j] == NUL {
-                    pInx = j
-                    sizeStr = string(b[i+1 : j])
-                    return
-                }
-            }
-        }
-    }
-    return
-}
-*/
