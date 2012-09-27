d97e57fb8814dbabddc99181a3a4a8654b7e1f8c
Michael Bosworth <michael.a.bosworth@gmail.com>
2012-09-26 21:32:55 -0400

    test tag's cat-file test tag's String()

--- api/tag_test.go
+++ api/tag_test.go
@@ -14,12 +14,45 @@ this is not a tag. it is a flying spaghetti monster.`
 
 var testTag string
 
+func withHeader(tagText string) string {
+	return fmt.Sprintf("tag %d\000%s", len(tagText), tagText)
+}
+
 func init() {
-	testTag = fmt.Sprintf("tag %d\000%s", len(testTagText), testTagText)
+	testTag = withHeader(testTagText)
 }
 
 func Test_parseTag(t *testing.T) {
-	r := readerForString(testTag)
+	tag, ok := parseTag(t, testTag)
+	assert(t, ok)
+	assert(t, tag.tag == "tattoo")
+	assert(t, tag.object.String() == "a53e5437def11c1eed3a4be1d45fba42e7582b03" )
+	assert(t, tag.message == "this is not a tag. it is a flying spaghetti monster.")
+	assert(t, tag.tagger.Email() == "michael.a.bosworth@gmail.com")
+	assert(t, tag.tagger.Name() == "Michael Bosworth")
+	assert(t, tag.tagger.Seconds() == 1348058205)
+	assert(t, tag.tagger.Offset() == int(-240))
+}
+
+func Test_tagString(t * testing.T) {
+	tag, ok := parseTag(t, testTag)
+	assert(t, ok)
+	s := tag.String()
+	tagData := withHeader(s)
+	var thereAndBackAgain *Tag
+	thereAndBackAgain, ok = parseTag(t, tagData)
+	assert(t, ok)
+	assert(t, tag.tag == thereAndBackAgain.tag)
+	assert(t, tag.object.String() == thereAndBackAgain.object.String())
+	assert(t, tag.message == thereAndBackAgain.message)
+	assert(t, tag.tagger.Email() == thereAndBackAgain.tagger.Email())
+	assert(t, tag.tagger.Name() == thereAndBackAgain.tagger.Name())
+	assert(t, tag.tagger.Seconds() == thereAndBackAgain.tagger.Seconds())
+	assert(t, tag.tagger.Offset() == thereAndBackAgain.tagger.Offset())
+}
+
+func parseTag(t *testing.T, s string) (tag *Tag, ok bool) {
+	r := readerForString(s)
 	p := newObjectParser(r)
 
 	parsed, err := p.ParsePayload()
@@ -30,14 +63,6 @@ func Test_parseTag(t *testing.T) {
 	assertf(t, err == nil, "failed due to error")
 	assert(t, parsed != nil, "failed because payload was nil")
 
-	tag, ok := parsed.(*Tag)
-
-	assert(t, ok)
-	assert(t, tag.tag == "tattoo")
-	assert(t, tag.object.String() == "a53e5437def11c1eed3a4be1d45fba42e7582b03" )
-	assert(t, tag.message == "this is not a tag. it is a flying spaghetti monster.")
-	assert(t, tag.tagger.Email() == "michael.a.bosworth@gmail.com")
-	assert(t, tag.tagger.Name() == "Michael Bosworth")
-	assert(t, tag.tagger.Seconds() == 1348058205)
-	assert(t, tag.tagger.Offset() == int(-240))
+	tag, ok = parsed.(*Tag)
+	return
 }
