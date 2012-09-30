e1beedd0667e88f2898fdc7a6fa203e3e5f7e209
Jake Brukhman <jbrukh@gmail.com>
2012-09-30 13:12:10 -0400

    Show ref command, pattern parameter.

--- builtin/show_ref.go
+++ builtin/show_ref.go
@@ -3,12 +3,15 @@ package builtin
 import (
 	"flag"
 	"fmt"
+	"github.com/jbrukh/ggit/api"
+	"strings"
 )
 
 type ShowRefBuiltin struct {
 	HelpInfo
 	flags     flag.FlagSet
 	flagQuiet bool
+	flagWhich bool
 }
 
 var ShowRef = &ShowRefBuiltin{
@@ -24,6 +27,8 @@ var ShowRef = &ShowRefBuiltin{
 
 func init() {
 	ShowRef.flags.BoolVar(&ShowRef.flagQuiet, "q", false, "Do not print any results to stdout.")
+	ShowRef.flags.BoolVar(&ShowRef.flagWhich, "which", false, "Show which refs are loose and which are packed.")
+
 	// add to command list
 	Add(ShowRef)
 }
@@ -33,19 +38,19 @@ func (b *ShowRefBuiltin) Info() *HelpInfo {
 }
 
 func (b *ShowRefBuiltin) Execute(p *Params, args []string) {
-	ShowRef.flags.Parse(args)
-	args = ShowRef.flags.Args()
+	b.flags.Parse(args)
+	args = b.flags.Args()
+
+	if b.flagWhich {
+		b.Which(p)
+		return
+	}
 
 	//fmt.Println("getting ", args)
 	if len(args) > 0 {
 		// we want to see a particular ref
-		which := args[0]
-		ref, e := p.Repo.PeelRef(which)
-		if e != nil {
-			// nothing to show
-			return
-		}
-		fmt.Fprintln(p.Wout, ref.String())
+		refstr := args[0]
+		b.WithSuffix(p, refstr)
 	} else {
 		if refs, e := p.Repo.Refs(); e != nil {
 			fmt.Fprintln(p.Werr, e.Error())
@@ -55,5 +60,52 @@ func (b *ShowRefBuiltin) Execute(p *Params, args []string) {
 				fmt.Fprintln(p.Wout, v.String())
 			}
 		}
+	}
+}
+
+func filterRefs(refs []api.Ref, f func(api.Ref) bool) []api.Ref {
+	r := make([]api.Ref, 0, len(refs))
+	for _, v := range refs {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
+
+func (b *ShowRefBuiltin) WithSuffix(p *Params, suffix string) {
+	refs, e := p.Repo.Refs()
+	if e != nil {
+		fmt.Fprintln(p.Werr, e.Error())
+	}
+	filtered := filterRefs(refs, func(ref api.Ref) bool {
+		return strings.HasSuffix(ref.Name(), suffix)
+	})
+	for _, v := range filtered {
+		fmt.Fprintln(p.Wout, v.String())
+	}
+}
+
+func (b *ShowRefBuiltin) Which(p *Params) {
+	repo := p.Repo.(*api.DiskRepository)
+
+	fmt.Fprintln(p.Wout, "Loose refs:")
+	refs, e := repo.LooseRefs()
+	if e != nil {
+		fmt.Fprint(p.Werr, e.Error())
+		return
+	}
+	for _, v := range refs {
+		fmt.Fprintln(p.Wout, v.String())
+	}
+
+	fmt.Fprintln(p.Wout, "\nPacked refs:")
+	prefs, e := repo.PackedRefs()
+	if e != nil {
+		fmt.Fprint(p.Werr, e.Error())
+		return
+	}
+	for _, v := range prefs {
+		fmt.Fprintln(p.Wout, v.String())
 	}
 }
