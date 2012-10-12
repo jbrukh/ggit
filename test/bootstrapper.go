89de9d400af113b3ab1e2e04a117fa3c4dcc84a2
Jake Brukhman <jbrukh@gmail.com>
2012-10-11 21:37:28 -0400

    Added first case.

--- test/bootstrapper.go
+++ test/bootstrapper.go
@@ -9,20 +9,22 @@ import (
 
 const varDir = "var"
 
-func CreateRepo(script string, name string) (api.Repository, error) {
+// CreateTestRepo creates a temporary directory and a subdirectory where
+// a test repo will be created. It passes this path to a script which
+// it executes. It then returns a ggit Repository based on that directory.
+func CreateTestRepo(script string, name string) (api.Repository, error) {
 	dir := path.Join(varDir, name)
-	if e := os.MkdirAll(dir, 0755); e != nil {
-		return nil, e
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
 	}
 	cmd := exec.Command(string, dir)
-	err := cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		return nil, err
 	}
 	return api.Open(dir)
 }
 
-func CleanRepo(name string) error {
+func CleanTestRepo(name string) error {
 	dir := path.Join(varDir, name)
 	return os.Remove(dir)
 }
