c4ac947827a06e6b3400399f12250798eda14a1f
Jake Brukhman <jbrukh@gmail.com>
2012-10-11 23:54:00 -0400

    Name change.

--- ggcase/ggcase.go
+++ ggcase/ggcase.go
@@ -7,19 +7,20 @@ import (
 	"os"
 )
 
+// ggcase lets you play around with test repository cases. It will
+// create that case that you specify in the working directory.
 func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
-		fmt.Fprintln(os.Stdout, "usage: ggtest <case_file>")
+		fmt.Fprintln(os.Stdout, "usage: ggcase <case_file>")
 		return
 	}
 	caseFile := args[0]
-
 	dir, err := test.Repo(".", caseFile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
-	fmt.Fprintf(os.Stdout, "Created case '%s' in '%s", caseFile, dir)
+	fmt.Fprintf(os.Stdout, "Created case '%s' in '%s'\n", caseFile, dir)
 }
