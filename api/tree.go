3cc179ded1eb46ca0de9bd753645adf6d0df4781
Jake Brukhman <jbrukh@gmail.com>
2012-10-14 19:41:17 -0400

    Added ggcompare.

--- api/tree.go
+++ api/tree.go
@@ -39,7 +39,7 @@ type TreeEntry struct {
 }
 
 func (e *TreeEntry) String() (s string) {
-	s = fmt.Sprintf("%.6o %s %-43s %s", e.mode, e.otype, e.oid, e.name)
+	s = fmt.Sprintf("%.6o %s %s\t%s", e.mode, e.otype, e.oid, e.name)
 	return
 }
 
@@ -79,7 +79,7 @@ func (p *objectParser) parseTree() *Tree {
 func (f *Format) Tree(t *Tree) (int, error) {
 	N := 0
 	for _, e := range t.entries {
-		n, err := fmt.Fprintf(f.Writer, "%.6o %s %-43s %s\n", e.mode, e.otype, e.oid, e.name)
+		n, err := fmt.Fprintf(f.Writer, "%.6o %s %s\t%s\n", e.mode, e.otype, e.oid, e.name)
 		N += n
 		if err != nil {
 			return N, err
---
+++ cmd/ggcompare/ggcompare.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jbrukh/ggit/api"
+	"github.com/jbrukh/ggit/util"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "usage: ggcompare <repo>\n")
+		os.Exit(1)
+	}
+
+	repoPath := args[0]
+	println(repoPath)
+
+	repo := api.Open(repoPath)
+
+	oids, e := repo.ObjectIds()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "could not open repo: %s\n", e.Error())
+		os.Exit(1)
+	}
+
+	for _, oid := range oids {
+		fmt.Println(oid)
+
+		// get the dashP from git
+		dashP, err := util.GitExec(repoPath, "cat-file", "-p", oid.String())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "git could not read object: %s\n", oid)
+			os.Exit(1)
+		}
+
+		var o api.Object
+		o, err = repo.ObjectFromOid(oid)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ggit could not read object: %s\n", oid)
+			os.Exit(1)
+		}
+
+		f := api.NewStrFormat()
+		f.Object(o)
+		str := f.String()
+		if str != dashP {
+			fmt.Printf("mismatch; expected '%d' but got '%d'\n", len(dashP), len(str))
+			i := 0
+			for {
+				if dashP[i] != str[i] {
+					break
+				}
+				i++
+			}
+			fmt.Println(dashP[:i])
+			fmt.Println(dashP[i:i+10], "\n", str[i:i+10])
+			fmt.Println("-=====---==-==")
+			fmt.Println(str)
+			os.Exit(2)
+		}
+	}
+}
