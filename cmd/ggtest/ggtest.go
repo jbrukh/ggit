22debafda397a519b9d723ef584c8162ab45f4c9
Jake Brukhman <jbrukh@gmail.com>
2012-10-27 16:01:59 -0400

    Added ggtest.

---
+++ cmd/ggtest/ggtest.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jbrukh/ggit/util"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("go", "test", "./...")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Println("Creating test case repos...\n")
+
+	// build the test case repos
+	err := util.CreateRepoTestCases()
+	defer func() {
+		fmt.Println("Removing test case repos.")
+		util.RemoveRepoTestCases()
+	}()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Done.")
+
+	fmt.Println("Running tests...")
+	cmd.Run()
+}
