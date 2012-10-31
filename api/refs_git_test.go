5ec946d1b1a1c8e53f86da6e8fe75d8a66c6918b
Jake Brukhman <brukhman@gmail.com>
2012-10-31 01:21:21 -0400

    Added refs git test.

---
+++ api/refs_git_test.go
@@ -0,0 +1,24 @@
+//
+// Unless otherwise noted, this project is licensed under the Creative
+// Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. Please
+// see the README file.
+//
+// Copyright (c) 2012 The ggit Authors
+//
+
+/*
+refs_git_test.go implements tests for reading conrete and symbolic refs.
+*/
+package api
+
+// import (
+// 	"github.com/jbrukh/ggit/test"
+// 	"testing"
+// )
+
+// func Test_refs(t *testing.T) {
+// 	testRepo := test.Refs
+
+// 	repo := Open(testRepo.Repo())
+// 	output := testRepo.Output().(*test.OutputRefs)
+// }
