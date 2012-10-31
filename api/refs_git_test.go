e118d5a80f628d18930bedf87f999d8d2b7931a0
Jake Brukhman <brukhman@gmail.com>
2012-10-31 10:45:45 -0400

    Added some more very basic ref tests (TODO).

--- api/refs_git_test.go
+++ api/refs_git_test.go
@@ -17,7 +17,7 @@ import (
 	"testing"
 )
 
-func Test_refs(t *testing.T) {
+func Test_fullPathRefs(t *testing.T) {
 	testRepo := test.Refs
 
 	repo := Open(testRepo.Repo())
@@ -28,8 +28,34 @@ func Test_refs(t *testing.T) {
 	testSymbolicRef(t, repo, output.SymbolicRef2, output.SymbolicRef2Target)
 
 	oid := OidNow(output.CommitOid)
-	testConcreteRef(t, repo, "refs/heads/master", oid)
+	//tagOid := OidNow(output.AnnTagOid)
+	var (
+		master = "refs/heads/master"
+		branch = "refs/heads/" + output.BranchName
+	//	annTag   = "refs/tags/" + output.AnnTagName
+	//	lightTag = "refs/tags/" + output.LightTagName
+	)
 
+	// test reading these full path refs directly from
+	// the repository files, loose or packed
+	testConcreteRef(t, repo, master, oid)
+	testConcreteRef(t, repo, branch, oid)
+	//testConcreteRef(t, repo, annTag, tagOid)
+	//testConcreteRef(t, repo, lightTag, oid)
+
+	// peel down these full refts
+	testOidRefFromRef(t, repo, master, oid)
+	testOidRefFromRef(t, repo, branch, oid)
+	//testOidRefFromRef(t, repo, annTag, oid)
+	//testOidRefFromRef(t, repo, lightTag, oid)
+
+}
+
+func testOidRefFromRef(t *testing.T, repo Repository, spec string, oid *ObjectId) {
+	ref, err := OidRefFromRef(repo, spec)
+	util.AssertNoErr(t, err)
+	util.AssertEqualString(t, ref.Name(), spec)
+	assertConcreteRef(t, ref, oid)
 }
 
 func testSymbolicRef(t *testing.T, repo Repository, spec string, expTarget string) {
@@ -48,11 +74,11 @@ func testSymbolicRef(t *testing.T, repo Repository, spec string, expTarget strin
 func testConcreteRef(t *testing.T, repo Repository, spec string, oid *ObjectId) {
 	ref, err := repo.Ref(spec)
 	util.AssertNoErr(t, err)
-
-	// assert the name sticks
 	util.AssertEqualString(t, ref.Name(), spec)
+	assertConcreteRef(t, ref, oid)
+}
 
-	// make sure target is symbolic and matches
+func assertConcreteRef(t *testing.T, ref Ref, oid *ObjectId) {
 	symbolic, target := ref.Target()
 	util.Assert(t, !symbolic)
 	util.AssertEqualString(t, target.(*ObjectId).String(), oid.String())
