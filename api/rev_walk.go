4231db7b6343ea027c9919cafc0614c3f8b064a2
Jake Brukhman <jbrukh@gmail.com>
2012-11-04 19:09:12 -0500

    Basic rev walk.

--- api/rev_walk.go
+++ api/rev_walk.go
@@ -6,3 +6,40 @@
 // Copyright (c) 2012 The ggit Authors
 //
 package api
+
+import (
+	"fmt"
+)
+
+type RevVisitor func(c *Commit) error
+
+var RevPrinter RevVisitor = func(c *Commit) error {
+	fmt.Println(c.ObjectId())
+	return nil
+}
+
+func RevWalkFromRevision(repo Repository, rev string, f RevVisitor) error {
+	root, err := CommitFromRevision(repo, rev)
+	if err != nil {
+		return err
+	}
+
+	current := root
+	for {
+		// apply the visitor
+		if err = f(current); err != nil {
+			return err
+		}
+
+		// proceed to the next commit
+		parents := current.Parents()
+		if len(parents) < 1 {
+			break
+		}
+		current, err = CommitNthParent(repo, current, 1)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
